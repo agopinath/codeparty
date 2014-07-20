1f5b6ddc502fae65fe04c63bdf2f8439c52021a8
Ajay Gopinath <ajay.gopinath@outlook.com>
2014-07-20 07:31:57 -0700

    working on first copy of lobby

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -3,14 +3,10 @@ function joinLobby() {
 		type: "POST",
 		url: "http://5d9ca87.ngrok.com/lobby/join",
 		success:function(data) {
-			if(data == "true") {
-				console.log("Logged in successfully!!");
-				location.href = "./dashboard.html";
-			}
-			else {
-				alert('Wrong username');
-			}
+			console.log(data);
+			location.href = "./lobby.html";
 		},
+		xhrFields: {withCredentials: true},
 		error:function(){
 			console.log("ERROR");
 		}
--- scripts/lobby.js
+++ scripts/lobby.js
@@ -1,18 +1,74 @@
+joinLobby();
+//var alertTimerId = setTimeout("showAlert()", 1000);
+(function showAlert( ) {setTimeout(function() {
+var lastLen = -1;
+	$.ajax({
+			type: "GET",
+			url: "http://5d9ca87.ngrok.com/lobby/getlist",
+			success:function(data) {
+				console.log(data);
+				if (data.length >= 4 ) {
+					//clearTimeout(alertTimerId);
+					//gameStart();
+				} else if(lastLen != data.length) {
+					updateLobby(data);
+					lastLen = data.length;
+				}
+				showAlert();
+			},
+			xhrFields: {withCredentials: true},
+			error:function(){
+				console.log("ERROR");
+			}
+		});
+}, 2000)})();
 
+var numRows = 4;
+function updateLobby(ids) {
+	console.log("updating!!: " + ids);
+
+	var table = document.getElementById("lobby-tbl");
+	console.log("tblrlen: " + table.rows.length);
+
+	table.innerHTML = "";
+
+	for (var i = 0; i < numRows; i++) {
+		var row = table.insertRow(i);
+		var cell1 = row.insertCell(0);
+		var cell2 = row.insertCell(1);
+
+		if(i < ids.length) { 
+			cell1.innerHTML = ids[i];
+			cell2.innerHTML = "<img src=\"./img/checkmark.png\" width='64' height='64'>";
+		} else {
+			cell1.innerHTML = "---";
+			cell2.innerHTML = "<img src=\"./img/loading-wheel.GIF\" width='64' height='64'>";
+		}
+	}
+}
+
+function gameStart() {
+	$.ajax({
+		type: "POST",
+		url: "http://5d9ca87.ngrok.com/lobby/gameStart",
+		success:function(data) {
+			console.log("GAME STARTING: " + data);
+		},
+		xhrFields: {withCredentials: true},
+		error:function(){
+			console.log("ERROR");
+		}
+	});
+}
 
 function joinLobby() {
 	$.ajax({
 		type: "POST",
 		url: "http://5d9ca87.ngrok.com/lobby/join",
 		success:function(data) {
-			if(data == "true") {
-				console.log("Logged in successfully!!");
-				location.href = "./dashboard.html";
-			}
-			else {
-				alert('Wrong username');
-			}
+			console.log(data);
 		},
+		xhrFields: {withCredentials: true},
 		error:function(){
 			console.log("ERROR");
 		}
